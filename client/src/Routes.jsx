dbfb82f8346ec2b65fb2f6600c230d966ec4e57a
samuel gitau <116079135+ngugilovesyou@users.noreply.github.com>
2026-01-20 18:25:55 +0300

    Fix import path for CleanersMarketplace component

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Container from './components/Container';
 import Sidebar from './components/Sidebar';
 import HardwareStoreSystem from './deepseek_javascript_20260102_c5f587.jsx';
-import CleanersMarketplace from "./Compamy.jsx"
+import CleanersMarketplace from "./Company.jsx"
 const routes = createBrowserRouter([
     {
         path: '/',
